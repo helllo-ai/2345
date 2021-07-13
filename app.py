789aaeabf77a1bb97962948977e3ee89afc64ac2
helllo-ai <71385601+helllo-ai@users.noreply.github.com>
2021-07-13 14:39:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from typing import List
+from flask import Flask,jsonify, request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'name': u'Raju',
+        'contact': u'9987644456',
+        'done': False
+    },
+    {
+        'id': 2,
+        'name': u'Rahul',
+        'contact': u'9887644456',
+        'done': False
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/add-data", methods=["POST"])
+def add_task():
+    if not request.json:
+        return jsonify({
+            "status":"error",
+            "message": "Please provide the data!"
+        },400)
+
+    contact = {
+        'id': tasks[-1]['id'] + 1,
+        'name': request.json['Name'],
+        'contact': request.json.get('Contact', ""),
+        'done': False
+    }
+    List.append(contact)
+    return jsonify({
+        "status":"success",
+        "message": "Contact added succesfully!"
+    })
+    
+
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : List
+    }) 
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
